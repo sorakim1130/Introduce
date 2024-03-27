2f74bf61dc8a02b228b9b99c07b5b805c4695a38
sorakim <sora6418@gmail.com>
2024-03-27 12:44:20 +0900

    fix: build

--- src/views/Home/components/Section/index.tsx
+++ src/views/Home/components/Section/index.tsx
@@ -1,22 +1,24 @@
 import styled from "@emotion/styled";
-import {ISection} from "@/types/home";
+import { ISection } from "@/types/home";
 import Company from "@/views/Home/components/Section/Company";
-import {toyProjects} from "@/data/toyProjects";
+import { toyProjects } from "@/data/toyProjects";
 import ToyProject from "@/views/Home/components/ToyProject";
 import Skills from "@/views/Home/components/Section/Skills";
 
 interface Props {
-    data: ISection
+  data: ISection;
 }
 
-const Section = ({data}: Props) => {
-    return (
-        <Container>
-            <h2>{data.title}</h2>
-            {data.companies?.map((item) => <Company key={item.name} data={item}/>)}
-            {data.toyProjects?.map((item) => <ToyProject key={item.title} data={item}/>)}
-        </Container>
-    );
+const Section = ({ data }: Props) => {
+  return (
+    <Container>
+      <h2>{data.title}</h2>
+      {data.companies?.map((item) => <Company key={item.name} data={item} />)}
+      {data.toyProjects?.map((item) => (
+        <ToyProject key={item.title} data={item} />
+      ))}
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -28,4 +30,4 @@ const Container = styled.div`
   }
 `;
 
-export default Section
+export default Section;
